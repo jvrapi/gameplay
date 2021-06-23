2e3cea8c986140994c844383f521dbb450486b69
João Vitor <joaooviitorr@hotmail.com>
2021-06-23 10:36:15 -0300

    Aula 02; Adicionado fontes e linear gradient; Atualizado estilos pagina de sign in; Aplicado linear gradient no aplicativo;

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -8,17 +8,16 @@ export function SignIn() {
 
 	return (
 		<View style={styles.container}>
-			<StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 			<Image source={IllustrationImg} style={styles.image} resizeMode='stretch' />
 			<View style={styles.content}>
 				<Text style={styles.title}>
-					Conecte-se {`\n`}
-					e organize suas {`\n`}
+					Conecte-se {'\n'}
+					e organize suas {'\n'}
 					jogatinas
 				</Text>
 
 				<Text style={styles.subtitle}>
-					Crie grupos para jogar seus games {`\n`}
+					Crie grupos para jogar seus games {'\n'}
 					favoritos com seus amigos
 				</Text>
 				<ButtonIcon title='Entrar com Discord' activeOpacity={0.7} />
